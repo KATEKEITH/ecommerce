531d3f18da488c7c340c247d03377ee7ab3c8c12
KATEKEITH <46472768+KATEKEITH@users.noreply.github.com>
2023-09-04 12:03:12 +0900

    #5 Prometheus + Grafana

---
+++ commerce/src/main/java/com/kate/commerce/controller/MetricsController.java
@@ -0,0 +1,17 @@
+package com.kate.commerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MetricsController {
+    @GetMapping("/end-point1")
+    public String endPoint1() {
+        return "Metrics for endPoint1";
+    }
+
+    @GetMapping("/end-point2")
+    public String endpoint2() {
+        return "Metrics for endPoint2";
+    }
+}
